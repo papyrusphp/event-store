95a017a2593a7bc0bf9a743c528362dc7254a582
Jeroen de Graaf <hello@jero.work>
2022-10-09 14:47:04 +0200

    Update AggregateRootRepository interface; add @throws
    AggregateRoot not found is a valid outcome for this contract.

--- src/Repository/AggregateRootRepository.php
+++ src/Repository/AggregateRootRepository.php
@@ -6,11 +6,14 @@ namespace Papyrus\EventStore\Repository;
 
 use Papyrus\EventSourcing\AggregateRoot;
 use Papyrus\EventSourcing\AggregateRootId;
+use Papyrus\EventStore\EventStore\AggregateRootNotFoundException;
 
 interface AggregateRootRepository
 {
     /**
      * @param class-string<AggregateRoot> $aggregateRootClassName
+     *
+     * @throws AggregateRootNotFoundException
      */
     public function get(string $aggregateRootClassName, AggregateRootId $aggregateRootId): AggregateRoot;
 
